34028d3265cba48fa2085096661f954def9c8230
syedincharge <rizvisyedba@gmail.com>
2022-12-06 16:46:28 -0600

    Initial commit

---
+++ src/main/java/com/rizvi/spring/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.rizvi.spring.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public ModelAndView showMessage(@RequestParam(value = "name",required = false, defaultValue = "World") String name){
+        System.out.println("From Controller---->>>");
+        ModelAndView mv = new ModelAndView("hello");
+        mv.addObject("hello", "Hello");
+        mv.addObject("name", name);
+        return  mv;
+
+    }
+}
